aa24e5654fe95d3657e303f34fe8753c833df9a5
Ricardo Lira da Fonseca <rilifon@fortran.linux.ime.usp.br>
2015-09-21 12:40:16 -0300

    First Fit improved melhorado a iteração pelo vetor (vetor marca o tamanho dos processos e vai pulando)

--- ep2-renato-ricardo/mem_mgr.cpp
+++ ep2-renato-ricardo/mem_mgr.cpp
@@ -4,37 +4,37 @@
 int ff(int size) {
 	int i, j, fit;
 	/*Percorre a memória total*/
-	for(i = 0; i < t_size; i++) {
+	for(i = 0; i + size < t_size; i+= t_memory[i]) {
 		if(!memory[i]) {
 			/*Checa se o tamanho livre é o suficiente*/
 			for(j = 1, fit = true; j < size; j++){
 				if(t_memory[i+j]) {
+					i = i+j;
 					fit = false;
 					break;
 				}
 			}
 			/*Se couber, marca que esta ocupado*/
 			if(fit){
-				for(j = i; j-i< size; j++)
-					memory[j] = 1;
+				memory[i] = size;
 				return(i);
 			}
 		}
 	}
 	/*Percorre a memória virtual*/
-	for(i = 0; i < v_size; i++) {
+	for(i = 0; i + size < v_size; i+= v_memory[i]) {
 		if(!memory[i]) {
 			/*Checa se o tamanho livre é o suficiente*/
-			for(j = 1, fit = true; j < size; j++) {
+			for(j = 1, fit = true; j < size; j++){
 				if(v_memory[i+j]) {
+					i = i+j;
 					fit = false;
 					break;
 				}
 			}
 			/*Se couber, marca que esta ocupado*/
 			if(fit){
-				for(j = i; j-i< size; j++)
-					memory[j] = 1;
+				memory[i] = size;
 				return(i);
 			}
 		}
