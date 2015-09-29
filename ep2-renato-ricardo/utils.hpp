d17aa51846c0133009371dd04ae23979b39c0cc4
rilifon <pudimdefrutas@gmail.com>
2015-09-29 14:59:20 -0300

    utils com struct da memória

--- ep2-renato-ricardo/utils.hpp
+++ ep2-renato-ricardo/utils.hpp
@@ -1,8 +1,19 @@
 #ifndef _UTILS_
 #define _UTILS_
 
+struct Mem_node{
+	char type; /*P se for um processo, L se for livre*/
+	Mem_node *ini; /*Endereço de início da memória*/
+	int size; /*Tamanho da memória*/
+	Mem_node *next; /*Próximo chunk de memória*/
+};
+
+
+
 int mem_size;
 
 int *memory;
+
+
 
 #endif /* _UTILS_ */
