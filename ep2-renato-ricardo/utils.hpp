3ac5c1bd50ca8f3b09dd15f41c065e91d52b40a0
Renato Lui Geh <renatogeh@gmail.com>
2015-09-29 15:14:09 -0300

    mem_node arrumado.

--- ep2-renato-ricardo/utils.hpp
+++ ep2-renato-ricardo/utils.hpp
@@ -1,19 +1,30 @@
-#ifndef _UTILS_
-#define _UTILS_
-
-struct Mem_node{
-	char type; /*P se for um processo, L se for livre*/
-	Mem_node *ini; /*Endereço de início da memória*/
-	int size; /*Tamanho da memória*/
-	Mem_node *next; /*Próximo chunk de memória*/
-};
-
-
+#ifndef _UTILS_HPP
+#define _UTILS_HPP
 
 int mem_size;
 
-int *memory;
+struct mem_node {
+  /* Inicio do bloco. */
+  int i;
+  /* Tamanho do bloco. */
+  int s;
+  /* Proximo bloco. */
+  mem_node *n;
+  /* Bloco anterior. */
+  mem_node *p;
+  /* Constructors. */
+  mem_node(int _i, int _s) : i(_i), s(_s) {}
+  mem_node(int _i, int _s, mem_node *_n, mem_node *_p) : i(_i), s(_s), n(_n), p(_p) {}
+};
 
 
+int t_size;
+int v_size;
 
-#endif /* _UTILS_ */
+int *t_memory;
+int *v_memory;
+
+mem_node *t_mem_h;
+mem_node *v_mem_h;
+
+#endif /* _UTILS_HPP */
